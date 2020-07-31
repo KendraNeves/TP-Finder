f6da3e4e06c6f1b176e88d27c95ddbfbc427db9a
Dmitri Friedenberg <dmitrifriedenberg@gmail.com>
2020-07-31 10:46:23 -0400

    Added link to change store status from location

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -34,7 +34,11 @@ module.exports = function(app) {
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get("/members", isAuthenticated, function(req, res) {
-    res.render(path.join(__dirname, "../views/members"), {title: "members"});
+    res.render(path.join(__dirname, "../views/members"), {title: "members", storeID: null});
+  });
+  
+  app.get("/members/:storeID", isAuthenticated, function(req, res) {
+    res.render(path.join(__dirname, "../views/members"), {title: "members", storeID: req.params.storeID});
   });
 
 };
